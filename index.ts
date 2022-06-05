f2325519f6164ccbc24dddc51bd7b5a96fbd70ee
Benni-beep <krikelkr4kel@gmail.com>
2022-06-05 07:06:59 +0200

    added try..catch to getCreatureCr will throw new Err if creatureName is not foudn in any book -> returns only promise<string> now

--- index.ts
+++ index.ts
@@ -29,79 +29,86 @@ type CreatureStatBlock = { name: string; cr: string };
 // type creatureCr = keyof powerlevelByCr;
 // type playerPowerlevel = (valueof?) powerlevelByPlayerLevel;
 
-// const party = {
-//   playerCharacters: ["Linea", "Nix", "Ninas_Char", "Fabians_Char", "Dörte"],
-//   level: 4,
-// };
+const party = {
+  playerCharacters: ["Linea", "Nix", "Ninas_Char", "Fabians_Char", "Dörte"],
+  level: 4,
+};
 
-// const allMobs = [
-//   {
-//     name: "kobold",
-//     mobSize: 8,
-//   },
-//   {
-//     name: "kobold sorcerer",
-//     mobSize: 1,
-//   },
-//   {
-//     name: "kobold inventor",
-//     mobSize: 1,
-//   },
-// ];
+const allMobs = [
+  {
+    name: "kobold",
+    mobSize: 8,
+  },
+  {
+    name: "kobold sorcerer",
+    mobSize: 1,
+  },
+  {
+    name: "kobold inventor",
+    mobSize: 1,
+  },
+];
 
-// const getPartyPowerlevel = (party: Party) => {
-//   const partySize: number = party.playerCharacters.length;
-//   const partyLevel: number = party.level;
-//   const powerLevelPerCharacter: number = powerlevelByPlayerLevel[partyLevel];
-//   const partyPowerlevel: number = partySize * powerLevelPerCharacter;
-//   return partyPowerlevel;
-// };
+const getPartyPowerlevel = (party: Party) => {
+  const partySize: number = party.playerCharacters.length;
+  const partyLevel: number = party.level;
+  const powerLevelPerCharacter: number = powerlevelByPlayerLevel[partyLevel];
+  const partyPowerlevel: number = partySize * powerLevelPerCharacter;
+  return partyPowerlevel;
+};
 
-// const getCreaturePowerlevel = (creatureName: string) => {
-//   const creatureCr: number = getCreatureCr(creatureName);
-//   const creaturePowerlevel: number = powerlevelByCr[creatureCr];
-//   return creaturePowerlevel;
-// };
+const getCreaturePowerlevel = async (creatureName: string) => {
+  const creatureCr: string = await getCreatureCr(creatureName);
+  const creaturePowerlevel: number = powerlevelByCr[creatureCr];
+  return creaturePowerlevel;
+};
 
-// const getAllMobsPowerlevel = async (allMobs: AllMobs) => {
-//   return allMobs.reduce((powerlevelTotalOfAllMobs, mob) => {
-//     const creatureName = mob.name;
-//     const mobSize = mob.mobSize;
-//     const creaturePowerlevel = await getCreaturePowerlevel(creatureName);
-//     const mobPowerlevel = mobSize * creaturePowerlevel;
-//     return powerlevelTotalOfAllMobs + mobPowerlevel;
-//   }, 0);
-// };
+// FIXME!
 
-// const renderDifficulty = async () => {
-//   const powerlevelTotalOfAllMobs: number = await getAllMobsPowerlevel(allMobs);
-//   const partyPowerlevel: number = getPartyPowerlevel(party);
-//   const difficultyValue: number = powerlevelTotalOfAllMobs / partyPowerlevel;
+const getAllMobsPowerlevel = (allMobs: AllMobs) => {
+  // Types of parameters 'powerlevelTotalOfAllMobs' and 'previousValue' are incompatible.
+  // Type '{ name: string; mobSize: number; }' is not assignable to type 'number'.
+  // Type 'Promise<number>' is not assignable to type 'number'
 
-//   let description = "";
-//   if (difficultyValue <= 0.4) {
-//     description = "easy";
-//   } else if (difficultyValue <= 0.6) {
-//     description = "medium";
-//   } else if (difficultyValue <= 0.8) {
-//     description = "hard";
-//   } else if (difficultyValue <= 1) {
-//     description = "deadly";
-//   } else if (difficultyValue > 1) {
-//     description = "absurd";
-//   }
-//   return `${description} (${difficultyValue * 100}%)`;
-// };
 
-// // does nothing yet!
-// const mixedGroup = (allMobs: AllMobs, ...classLevelNPCs: classLevelNPCs) => {
-//   // const allMobsPowerlevel = getAllMobsPowerlevel(allMobs);
-//   // const allClassLevelNPCs = [...classLevelNPCs];
-//   // iterate over allClassLevelNPCs and sum their powerlevels
-//   // add this sum to allMobsPowerlevel and return
-// };
+  return allMobs.reduce(async (powerlevelTotalOfAllMobs, mob) => {
+    const creatureName = mob.name;
+    const mobSize = mob.mobSize;
+    const creaturePowerlevel = await getCreaturePowerlevel(creatureName);
+    const mobPowerlevel = mobSize * creaturePowerlevel;
+    return powerlevelTotalOfAllMobs + mobPowerlevel;
+  }, 0);
+};
 
-const getJson = (filePath: string) => {
+const renderDifficulty = async () => {
+  const powerlevelTotalOfAllMobs: number = await getAllMobsPowerlevel(allMobs);
+  const partyPowerlevel: number = getPartyPowerlevel(party);
+  const difficultyValue: number = powerlevelTotalOfAllMobs / partyPowerlevel;
+
+  let description = "";
+  if (difficultyValue <= 0.4) {
+    description = "easy";
+  } else if (difficultyValue <= 0.6) {
+    description = "medium";
+  } else if (difficultyValue <= 0.8) {
+    description = "hard";
+  } else if (difficultyValue <= 1) {
+    description = "deadly";
+  } else if (difficultyValue > 1) {
+    description = "absurd";
+  }
+  return `${description} (${difficultyValue * 100}%)`;
+};
+
+// does nothing yet!
+const mixedGroup = (allMobs: AllMobs, ...classLevelNPCs: classLevelNPCs) => {
+  // const allMobsPowerlevel = getAllMobsPowerlevel(allMobs);
+  // const allClassLevelNPCs = [...classLevelNPCs];
+  // iterate over allClassLevelNPCs and sum their powerlevels
+  // add this sum to allMobsPowerlevel and return
+};
+
+const getJson = (filePath: string)=> {
   return fs.readFile(filePath, { encoding: "utf-8" }).then((jsonString) => {
     return JSON.parse(jsonString);
   });
@@ -121,11 +128,12 @@ const getCreatureStatBlock = (
 };
 
 // FIXME: cr type !== string -> write type cr = keyof powerlevelByCr
+// FIXME: what happens if creature is found in none if the books? -> promise<undefined>?
 const getCreatureCr = async (creatureName: string) => {
   // iterate over files from allBestiaryFileNames[] in ./bestiary/*
   for (const bestiaryFileName of allBestiaryFileNames) {
     const filePath = `./bestiary/${bestiaryFileName}`;
-    // read  file, parse to JSON
+    // read file, parse to JSON
     const bestiaryJson = await getJson(filePath);
     // search if creature is in file, if yes, return cr and break loop
     const creatureStatblock = getCreatureStatBlock(creatureName, bestiaryJson);
@@ -134,12 +142,16 @@ const getCreatureCr = async (creatureName: string) => {
      return creatureStatblock.cr;
     }
   }
+  throw new Error("creatureName not found in any book")
 };
 
 const main = async () => {
-  const creatureName = "kobold";
+  try{
+  const creatureName = "alvnwl";
   const creatureCr = await getCreatureCr(creatureName);
-  console.log("creatureCr in main()", creatureCr);
+  console.log("creatureCr in main()", creatureCr);}
+  catch(err) {console.log(err);
+  }
 };
 
 main();
