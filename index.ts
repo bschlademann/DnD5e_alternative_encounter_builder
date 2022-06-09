1fdd4c02aedb1071e34a0cdb365d7b6f852b70ad
Benni-beep <krikelkr4kel@gmail.com>
2022-06-09 08:30:39 +0200

    added zod , refactoring added generator function {readFiles} to {getCreatureCr} removed unneeded types infered function return types

--- index.ts
+++ index.ts
@@ -4,61 +4,94 @@ import {
   allBestiaryFileNames,
 } from "./data.js";
 import * as fs from "fs/promises";
-import { stringify } from "querystring";
+import * as z from "zod";
 
 type Party = { playerCharacters: string[]; level: number };
 type Mob = { creatureName: string; mobSize: number };
-type AllMobs = Mob[] | [];
 type LeveledNPC = { name: string; level: number };
-type AllLeveledNPCs = LeveledNPC[] | [];
 type BestiaryJson = { monster: { name: string; cr: string }[] };
 type CreatureStatBlock = { name: string; cr: string };
 
 const party: Party = {
-  playerCharacters: ["Linea", "Nix", "Noa", "Fabians_Char", "Dörte"],
+  playerCharacters: ["Linea", "Nix", "Noa", "Golan", "Dörte"],
   level: 4,
 };
-const allMobs: AllMobs | [] = [
+
+const allMobs: Mob[] = [
   // {
   //   creatureName: "monodrone",
   //   mobSize: 4,
   // },
-  {
-    creatureName: "duodrone",
-    mobSize: 1,
-  },
-  {
-    creatureName: "tridrone",
-    mobSize: 1,
-  },
-  {
-    creatureName: "quadrone",
-    mobSize: 2,
-  },
+  // {
+  //   creatureName: "duodrone",
+  //   mobSize: 1,
+  // },
+  // {
+  //   creatureName: "tridrone",
+  //   mobSize: 1,
+  // },
+  // {
+  //   creatureName: "quadrone",
+  //   mobSize: 2,
+  // },
+  // {
+  //   creatureName: "kobold scale sorcerer",
+  //   mobSize: 1,
+  // },
+  // {
+  //   creatureName: "kobold inventor",
+  //   mobSize: 1,
+  // },
+  // {
+  //   creatureName: "kobold dragonshield",
+  //   mobSize: 3,
+  // },
+  // {
+  //   creatureName: "kobold",
+  //   mobSize: 9,
+  // },
 ];
-const allLeveledNPCs: AllLeveledNPCs = [
-  { name: "Dolgrim", level: 5 },
+
+const allLeveledNPCs: LeveledNPC[] = [
+  // { name: "Dolgrim", level: 5 },
   // { name: "Ilyas", level: 5 },
   // { name: "Bres", level: 5 },
   // { name: "Nif", level: 5 },
 ];
 
-const getPartyPowerlevel = (party: Party) => {
-  const partySize: number = party.playerCharacters.length;
-  const partyLevel: number = party.level;
-  const powerLevelPerCharacter: number = powerlevelByPlayerLevel[partyLevel];
-  const partyPowerlevel: number = partySize * powerLevelPerCharacter;
+const getPartyPowerlevel = (party: Party): number => {
+  const partySize = party.playerCharacters.length;
+  const powerLevelPerCharacter = powerlevelByPlayerLevel[party.level];
+  const partyPowerlevel = partySize * powerLevelPerCharacter;
   return partyPowerlevel;
 };
-const getJson = (filePath: string) => {
+
+// immer parsen oder validieren, wenn ich daten von außen rein bekomme
+const bestiaryJsonSchema: z.ZodSchema<BestiaryJson> = z.object({
+  monster: z.array(
+    z.object({ 
+        cr: z.string(), 
+        name: z.string() 
+    })
+  ),
+});
+
+const isBestiaryJson = (u: unknown): u is BestiaryJson =>
+  bestiaryJsonSchema.safeParse(u).success;
+
+const parseBestiaryJson = (u: unknown): BestiaryJson =>
+  bestiaryJsonSchema.parse(u);
+
+const getJson = (filePath: string): Promise<BestiaryJson> => {
   return fs.readFile(filePath, { encoding: "utf-8" }).then((jsonString) => {
-    return JSON.parse(jsonString);
+    return parseBestiaryJson(JSON.parse(jsonString));
   });
 };
+
 const getCreatureStatBlock = (
   creatureName: string,
   bestiaryJson: BestiaryJson
-) => {
+): false | { name: string; cr: string } => {
   const creatureStatBlock = bestiaryJson.monster.find(
     (statBlock: CreatureStatBlock) =>
       statBlock.name.toLocaleLowerCase() === creatureName.toLocaleLowerCase()
@@ -67,77 +100,107 @@ const getCreatureStatBlock = (
     return false;
   } else return creatureStatBlock;
 };
-const parseCr = (crString: string) => {
-  const isFraction = crString.includes("/");
-  if (isFraction) {
-    const split = crString.split("");
-    const onlystringifiedNumbers = split.filter(
-      (stringifiedNumber) => stringifiedNumber !== "/"
-    );
-    const parsedNumbers = onlystringifiedNumbers.map((stringifiedNumber) =>
-      parseInt(stringifiedNumber)
-    );
-    const parsedCr = parsedNumbers[0] / parsedNumbers[1];
-    return parsedCr;
-  } else {
-    const parsedCr = parseInt(crString);
-    return parsedCr;
-  }
+
+const stringToNumber = z.string().regex(/^\d+$/).transform(Number);
+
+
+const parseCr = (crString: string): number => {
+  const split = crString.split("/");
+  const fraction = z.union([
+    z.tuple([stringToNumber, stringToNumber]).transform(([a, b]) => a/b),
+    z.tuple([stringToNumber]).transform(([a]) => a),
+  ]);
+  
+  return fraction.parse(split);
+
+
+  // const isFraction = crString.includes("/");
+  // if (isFraction) {
+  //   const split = crString.split("/");
+  //   const parsedNumbers = split.map((stringifiedNumber) =>
+  //     parseInt(stringifiedNumber)
+  //   );
+  //   const parsedCr = parsedNumbers[0] / parsedNumbers[1];
+  //   return parsedCr;
+  // } else {
+  //   const parsedCr = parseInt(crString);
+  //   return parsedCr;
+  // }
 };
-const getCreatureCr = async (creatureName: string) => {
+
+async function* readFiles (creatureName: string) {
   for (const bestiaryFileName of allBestiaryFileNames) {
     const filePath = `./bestiary/${bestiaryFileName}`;
-    const bestiaryJson = await getJson(filePath);
+    yield await getJson(filePath);
+  }
+};
+
+const getCreatureCr = async (creatureName: string): Promise<number> => {
+  for await (const bestiaryJson of readFiles(creatureName)) {
     const creatureStatblock = getCreatureStatBlock(creatureName, bestiaryJson);
     if (creatureStatblock) {
       const parsedCr = parseCr(creatureStatblock.cr);
       return parsedCr;
     }
   }
+
+  // for (const bestiaryFileName of allBestiaryFileNames) {
+  //   const filePath = `./bestiary/${bestiaryFileName}`;
+  //   const bestiaryJson = await getJson(filePath);
+  //   const creatureStatblock = getCreatureStatBlock(creatureName, bestiaryJson);
+  //   if (creatureStatblock) {
+  //     const parsedCr = parseCr(creatureStatblock.cr);
+  //     return parsedCr;
+  //   }
+  // }
   throw new Error(`ERROR: "${creatureName}" not found in any book`);
 };
+
 const getCreaturePowerlevel = async (creatureName: string) => {
   const creatureCr = await getCreatureCr(creatureName);
   const creaturePowerlevel: number = powerlevelByCr[creatureCr];
   return creaturePowerlevel;
 };
-const getAllMobsPowerlevel = async (allMobs: AllMobs) => {
+
+const getAllMobsPowerlevel = async (allMobs: Mob[]) => {
   let powerlevelTotalOfAllMobs = 0;
-  const mobsPresent = allMobs.length > 0;
-  if (mobsPresent) {
-    for (const mob of allMobs) {
-      const { creatureName, mobSize } = mob;
-      const creaturePowerlevel = await getCreaturePowerlevel(creatureName);
-      const mobPowerlevel = mobSize * creaturePowerlevel;
-      powerlevelTotalOfAllMobs += mobPowerlevel;
-      console.log(`
-        creatureName:${creatureName}
-        mobSize:${mobSize}
-        creaturePowerlevel:${creaturePowerlevel}
-        mobPowerlevel:${mobPowerlevel}
-        new powerlevelTotalOfAllMobs: ${powerlevelTotalOfAllMobs}
-        `);
-    }
+  for (const mob of allMobs) {
+    const { creatureName, mobSize } = mob;
+    const creaturePowerlevel = await getCreaturePowerlevel(creatureName);
+    const mobPowerlevel = mobSize * creaturePowerlevel;
+    powerlevelTotalOfAllMobs += mobPowerlevel;
+    console.log(`
+      creatureName:${creatureName}
+      mobSize:${mobSize}
+      creaturePowerlevel:${creaturePowerlevel}
+      mobPowerlevel:${mobPowerlevel}
+      new powerlevelTotalOfAllMobs: ${powerlevelTotalOfAllMobs}
+      `);
   }
   return powerlevelTotalOfAllMobs;
 };
-const getAllLeveledNPCsPowerlevel = (allLeveledNPCs: AllLeveledNPCs) => {
+
+const getAllLeveledNPCsPowerlevel = (allLeveledNPCs: LeveledNPC[]) => {
+
+  return allLeveledNPCs.reduce((allLeveledNPCsPowerlevel, leveledNPC) => {
+    const powerlevel = powerlevelByPlayerLevel[leveledNPC.level];
+    return allLeveledNPCsPowerlevel += powerlevel;
+  },0)
+
   let allLeveledNPCsPowerlevel = 0;
-  const leveledNPCsPresent = allLeveledNPCs.length > 0;
-  if (leveledNPCsPresent) {
-    for (const leveledNPC of allLeveledNPCs) {
-      const { level } = leveledNPC;
-      const powerlevel = powerlevelByPlayerLevel[level];
-      allLeveledNPCsPowerlevel += powerlevel;
-      console.log(`
-      leveledNPC:${leveledNPC.name}
-      level:${level}
-      new allLeveledNPCsPowerlevel: ${allLeveledNPCsPowerlevel}
-      `);
-    }
+  for (const leveledNPC of allLeveledNPCs) {
+    const { level } = leveledNPC;
+    const powerlevel = powerlevelByPlayerLevel[level];
+    allLeveledNPCsPowerlevel += powerlevel;
+    console.log(`
+    leveledNPC:${leveledNPC.name}
+    level:${level}
+    new allLeveledNPCsPowerlevel: ${allLeveledNPCsPowerlevel}
+    `);
   }
   return allLeveledNPCsPowerlevel;
 };
+
 
 const difficulty = async () => {
   const powerlevelTotalOfAllMobs = await getAllMobsPowerlevel(allMobs);
@@ -170,4 +233,5 @@ const main = async () => {
   const result = await difficulty();
   console.log(result);
 };
+
 main();
