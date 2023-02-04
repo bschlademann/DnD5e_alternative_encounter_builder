e975a9615de5bbc90dc9402df3ef8ce3ca96cba9
Benni-beep <krikelkr4kel@gmail.com>
2023-02-04 18:23:58 +0100

    fix: move functions to local-storage.ts

--- src/lib.ts
+++ src/lib.ts
@@ -2,11 +2,4 @@ export const getJsonFromUrl = (repoUrl: string) => {
   return fetch(repoUrl).then((res) => res.json());
 };
 
-export const setLocalStorage = (data: unknown) => (key: string) =>
-  localStorage.setItem(key, JSON.stringify(data));
-
-export const getLocalStorage = (key: string) => {
-  localStorage.getItem(key);
-};
-
 export const getSetArray = (arr: any[]) => Array.from(new Set(arr));
