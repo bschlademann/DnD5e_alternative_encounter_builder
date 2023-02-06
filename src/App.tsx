6d4b17b6c7e1256a4cbc53fab4de584c36e5f1a9
Benni-beep <krikelkr4kel@gmail.com>
2023-02-06 12:47:11 +0100

    feat: add mock-inputs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,34 @@ import "./App.css";
 
 function App() {
   const [state, setState] = useState({});
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <form action="">
+        <div className="party">
+          <label htmlFor="party-number">party-number</label>
+          <input type="number" id="party-number" />
+          <label htmlFor="party-level">party-average-level</label>
+          <input type="number" id="party-level" />
+        </div>
+
+        <div className="creature">
+          <label htmlFor="creature-name">creature-name</label>
+          <input type="text" id="creature-name" />
+          <label htmlFor="creature-number">creature-number</label>
+          <input type="number" id="creature-number" />
+        </div>
+
+        <div className="named-npcs">
+          <label htmlFor="named-npcs-number">named-npcs-number</label>
+          <input type="number" id="named-npcs-number" />
+          <label htmlFor="named-npcs-level">named-npcs-level</label>
+          <input type="number" id="named-npcs-level" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
