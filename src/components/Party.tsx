f4084553c78b4ac21cdb0f7457816bc3eee13b76
Benni-beep <krikelkr4kel@gmail.com>
2023-02-13 13:41:42 +0100

    refactor: extract change handlers

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext } from "react";
 import { getRange } from "../lib";
 
 const Option = (props: { value: number }) => {
@@ -18,37 +18,39 @@ export const PartyContext = createContext<
 
 export const Party = () => {
   const [party, setParty] = useContext(PartyContext);
+  const onChangeCount = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setParty((prevState) => {
+        return { ...prevState, count: parseInt(e.target.value) };
+      });
+    },
+    []
+  );
+
+  const onChangeLevel = React.useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setParty((prevState) => {
+        return { ...prevState, level: parseInt(e.target.value) };
+      });
+    },
+    []
+  );
+
   const validCounts = getRange([1, 12]);
   const validLevels = getRange([1, 20]);
   return (
     <div className="party">
       <label htmlFor="character-count">number of characters</label>
-      <select
-        id="character-count"
-        value={party.count}
-        onChange={(e) =>
-          setParty((prevState) => {
-            return { ...prevState, count: parseInt(e.target.value) };
-          })
-        }
-      >
+      <select id="character-count" value={party.count} onChange={onChangeCount}>
         {validCounts.map((value) => (
-          <Option value={value} key={value} />
+          <Option value={value} key={`count-${value}`} />
         ))}
       </select>
 
       <label htmlFor="character-level">number of characters</label>
-      <select
-        id="character-count"
-        value={party.level}
-        onChange={(e) =>
-          setParty((prevState) => {
-            return { ...prevState, level: parseInt(e.target.value) };
-          })
-        }
-      >
+      <select id="character-count" value={party.level} onChange={onChangeLevel}>
         {validLevels.map((value) => (
-          <Option value={value} key={value} />
+          <Option value={value} key={`level-${value}`} />
         ))}
       </select>
     </div>
