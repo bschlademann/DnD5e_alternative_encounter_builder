447782c27e4a770edac582289b9d94052ed8ab1f
Benni-beep <krikelkr4kel@gmail.com>
2023-02-14 18:55:04 +0100

    feat: add MobsContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import "./App.css";
 import type { Creature } from "./5etools";
 
 import { Party, PartyContext } from "./components/Party";
-import { CreatureContext, CreatureSelector } from "./components/CreatureSelector";
+import {
+  CreatureContext,
+  CreatureSelector,
+  MobsContext,
+} from "./components/CreatureSelector";
+import { Mob } from "./domain";
 
 function App() {
   const [party, setParty] = useState({ count: 1, level: 1 });
   const [creatures, setCreatures] = useState<Creature[]>([]);
+  const [mobs, setMobs] = useState<Mob[]>([]);
 
   useEffect(() => {
     // localStorage.clear();
@@ -16,14 +22,16 @@ function App() {
   }, []);
 
   return (
-    <CreatureContext.Provider value={creatures}>
-      <PartyContext.Provider value={[party, setParty]}>
-        <div className="App">
-          <Party />
-          <CreatureSelector />
-        </div>
-      </PartyContext.Provider>
-    </CreatureContext.Provider>
+    <MobsContext.Provider value={[mobs, setMobs]}>
+      <CreatureContext.Provider value={creatures}>
+        <PartyContext.Provider value={[party, setParty]}>
+          <div className="App">
+            <Party />
+            <CreatureSelector />
+          </div>
+        </PartyContext.Provider>
+      </CreatureContext.Provider>
+    </MobsContext.Provider>
   );
 }
 
--- src/components/creatureSelector.tsx
+++ src/components/creatureSelector.tsx
@@ -1,20 +1,34 @@
 import { createContext, useContext, useState } from "react";
 import { Creature } from "../5etools";
+import { Mob } from "../domain";
 
 export const CreatureContext = createContext<Creature[]>([]);
+export const MobsContext = createContext<
+  [Mob[], React.Dispatch<React.SetStateAction<Mob[]>>]
+>([[], () => undefined]);
 
 export const CreatureSelector = () => {
   const [filterQuery, setFilterQuery] = useState("");
   const creatures = useContext(CreatureContext);
-  const filteredCreatures = creatures.filter(creature => creature.name.toLowerCase().includes(filterQuery));
-  const filterCreatureNames = (e: React.ChangeEvent<HTMLInputElement>) => setFilterQuery(e.target.value);
+  const [mobs, setMobs] = useContext(MobsContext);
+  const filteredCreatures = creatures.filter((creature) =>
+    creature.name.toLowerCase().includes(filterQuery)
+  );
+  const filterCreatureNames = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFilterQuery(e.target.value);
 
   return (
     <div className="creature-selector">
-      <input type="text" placeholder="enter creature name" value={filterQuery} onChange={filterCreatureNames}/>
+      <input
+        type="text"
+        placeholder="enter creature name"
+        value={filterQuery}
+        onChange={filterCreatureNames}
+      />
       <table>
         <thead>
           <tr>
+            <th></th>
             <th>name</th>
             <th>cr</th>
           </tr>
@@ -23,6 +37,8 @@ export const CreatureSelector = () => {
           {filteredCreatures.map((creature) => {
             return (
               <tr>
+                <button>+</button>
+                <button>-</button>
                 <td>{creature.name}</td>
                 <td>{creature.cr}</td>
               </tr>
