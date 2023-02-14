f9aaee28ddd8134f021493f5adbd3e0dbf3122fa
Benni-beep <krikelkr4kel@gmail.com>
2023-02-14 18:19:42 +0100

    feat: filter table via input value

--- src/components/creatureSelector.tsx
+++ src/components/creatureSelector.tsx
@@ -1,13 +1,17 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { Creature } from "../5etools";
 
 export const CreatureContext = createContext<Creature[]>([]);
 
 export const CreatureSelector = () => {
+  const [filterQuery, setFilterQuery] = useState("");
   const creatures = useContext(CreatureContext);
+  const filteredCreatures = creatures.filter(creature => creature.name.toLowerCase().includes(filterQuery));
+  const filterCreatureNames = (e: React.ChangeEvent<HTMLInputElement>) => setFilterQuery(e.target.value);
+
   return (
     <div className="creature-selector">
-      <input type="text" placeholder="enter creature name" value={0} />
+      <input type="text" placeholder="enter creature name" value={filterQuery} onChange={filterCreatureNames}/>
       <table>
         <thead>
           <tr>
@@ -16,7 +20,7 @@ export const CreatureSelector = () => {
           </tr>
         </thead>
         <tbody>
-          {creatures.map((creature) => {
+          {filteredCreatures.map((creature) => {
             return (
               <tr>
                 <td>{creature.name}</td>
