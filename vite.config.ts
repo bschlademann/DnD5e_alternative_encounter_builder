0bfcab5973b77384665874156f349915959fd6d1
Benni-beep <krikelkr4kel@gmail.com>
2023-02-21 19:29:24 +0100

    fix: pages repo base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/DnD5e_alternative_encounter_builder/",
   build: { outDir: "docs" },
 });
