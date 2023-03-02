bf0f6b33dc7344129dafe9cd11cc93dc5e33a37d
Benni-beep <krikelkr4kel@gmail.com>
2023-03-02 09:41:56 +0100

    refactor: extract contextproviders from contexts to fix h,r infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,60 @@
 // import "./App.css";
+import styles from "./Layout.module.css";
 
 import { Party } from "./components/Party";
 import { MobsSelector, Mob } from "./components/MobsSelector";
-import { Contexts } from "./contexts";
+import {
+  CreatureContext,
+  CreaturesByIdContext,
+  MobsContext,
+  PartyContext,
+} from "./contexts";
 import { Difficulty } from "./components/Difficulty";
+import { MobsList } from "./components/MobsList";
+import { useState, useEffect } from "react";
+import { Creature } from "./5etools";
+import { getCreaturesById } from "./domain";
+import { getOrUpdateLocalStorage } from "./local-storage";
 
 export type MobsState = { [creatureId: number]: Mob };
 
 function App() {
+  const [party, setParty] = useState({ count: 1, level: 1 });
+  const [creatures, setCreatures] = useState<Creature[]>([]);
+  const [mobs, setMobs] = useState<MobsState>({});
+  const [creaturesById, setCreaturesById] = useState({});
+  // const [leveledNpcs, setLeveledNpcs] = useState<LeveledNpcs>([]);
+
+  useEffect(() => {
+    // localStorage.clear();
+    getOrUpdateLocalStorage().then((creatures) => {
+      setCreatures(creatures);
+      const creaturesById = getCreaturesById(creatures);
+      setCreaturesById(creaturesById);
+    });
+  }, []);
+
   return (
-    <Contexts>
-      <Difficulty />
-      <Party />
-      <MobsSelector />
-    </Contexts>
+    // <LeveledNpcsContext.Provider value={[leveledNpcs, setLeveledNpcs]}>
+    <CreaturesByIdContext.Provider value={creaturesById}>
+      <MobsContext.Provider value={[mobs, setMobs]}>
+        <CreatureContext.Provider value={creatures}>
+          <PartyContext.Provider value={[party, setParty]}>
+            <div className={styles.container}>
+              <div className={styles.left}>
+                <MobsSelector />
+              </div>
+              <div className={styles.right}>
+                <MobsList />
+                <Party />
+                <Difficulty />
+              </div>
+            </div>
+          </PartyContext.Provider>
+        </CreatureContext.Provider>
+      </MobsContext.Provider>
+    </CreaturesByIdContext.Provider>
+    // </LeveledNpcsContext.Provider>
   );
 }
 
--- src/contexts.tsx
+++ src/contexts.tsx
@@ -21,34 +21,3 @@ export const PartyContext = createContext<
 // >([[], () => undefined]);
 
 export const CreaturesByIdContext = createContext<CreaturesById>({});
-
-export const Contexts:React.FC<{children: React.ReactNode}> = ({ children }) => {
-  const [party, setParty] = useState({ count: 1, level: 1 });
-  const [creatures, setCreatures] = useState<Creature[]>([]);
-  const [mobs, setMobs] = useState<MobsState>({});
-  const [creaturesById, setCreaturesById] = useState({});
-  // const [leveledNpcs, setLeveledNpcs] = useState<LeveledNpcs>([]);
-
-  useEffect(() => {
-    // localStorage.clear();
-    getOrUpdateLocalStorage().then((creatures) => {
-      setCreatures(creatures);
-      const creaturesById = getCreaturesById(creatures);
-      setCreaturesById(creaturesById);
-    });
-  }, []);
-
-  return (
-    // <LeveledNpcsContext.Provider value={[leveledNpcs, setLeveledNpcs]}>
-    <CreaturesByIdContext.Provider value={creaturesById}>
-      <MobsContext.Provider value={[mobs, setMobs]}>
-        <CreatureContext.Provider value={creatures}>
-          <PartyContext.Provider value={[party, setParty]}>
-            {children}
-          </PartyContext.Provider>
-        </CreatureContext.Provider>
-      </MobsContext.Provider>
-    </CreaturesByIdContext.Provider>
-    // </LeveledNpcsContext.Provider>
-  );
-}
