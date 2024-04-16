84172aad2dd0b9024d4d6a3cdf989540df4eeeee
Benni-beep <krikelkr4kel@gmail.com>
2024-04-16 08:42:43 +0200

    refactor: cleanup

--- src/components/Difficulty.tsx
+++ src/components/Difficulty.tsx
@@ -4,6 +4,6 @@ export const Difficulty = () => {
     const difficulty = getDifficulty()
     const formatedDifficulty = formatDifficultyOutput(difficulty)
 return (
-    <div>difficulty: {JSON.stringify(formatedDifficulty.difficulty)}</div>
+    <div>difficulty: {formatedDifficulty.difficulty}</div>
 )
 }
