3deea5c13bbd9111ff7c4e1315550dcd43fed637
Benni-beep <krikelkr4kel@gmail.com>
2024-05-27 08:04:00 +0200

    fix: move border pngs to public and change paths in BorderDecorations

--- src/components/BorderDecoration.tsx
+++ src/components/BorderDecoration.tsx
@@ -24,14 +24,38 @@ export const BorderDecoration = ({
 
   return (
     <div className="container">
-      <div className="upper left corner" style={{backgroundImage: `url("./assets/png/${upperLeftCorner}.png")`}}></div>
-      <div className="upper right corner" style={{backgroundImage: `url("/assets/png/${upperRightCorner}.png")`}}></div>
-      <div className="lower left corner" style={{backgroundImage: `url("./src/assets/png/${lowerLeftCorner}.png")`}}></div>
-      <div className="lower right corner" style={{backgroundImage: `url("./src/assets/png/${lowerRightCorner}.png")`}}></div>
-      <div className="upper edge" style={{backgroundImage: `url("./src/assets/png/${upperEdge}.png")`}}></div>
-      <div className="lower edge" style={{backgroundImage: `url("./src/assets/png/${lowerEdge}.png")`}}></div>
-      <div className="left edge" style={{backgroundImage: `url("./src/assets/png/${leftEdge}.png")`}}></div>
-      <div className="right edge" style={{backgroundImage: `url("./src/assets/png/${rightEdge}.png")`}}></div>
+      <div
+        className="upper left corner"
+        style={{ backgroundImage: `url("/png/${upperLeftCorner}.png")` }}
+      ></div>
+      <div
+        className="upper right corner"
+        style={{ backgroundImage: `url("/png/${upperRightCorner}.png")` }}
+      ></div>
+      <div
+        className="lower left corner"
+        style={{ backgroundImage: `url("/png/${lowerLeftCorner}.png")` }}
+      ></div>
+      <div
+        className="lower right corner"
+        style={{ backgroundImage: `url("/png/${lowerRightCorner}.png")` }}
+      ></div>
+      <div
+        className="upper edge"
+        style={{ backgroundImage: `url("/png/${upperEdge}.png")` }}
+      ></div>
+      <div
+        className="lower edge"
+        style={{ backgroundImage: `url("/png/${lowerEdge}.png")` }}
+      ></div>
+      <div
+        className="left edge"
+        style={{ backgroundImage: `url("/png/${leftEdge}.png")` }}
+      ></div>
+      <div
+        className="right edge"
+        style={{ backgroundImage: `url("/png/${rightEdge}.png")` }}
+      ></div>
       {children}
     </div>
   );
