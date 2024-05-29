d818a4a4f34ba8099dce819d216f73ae63bf35c9
Benni-beep <krikelkr4kel@gmail.com>
2024-05-29 19:08:11 +0200

    refactor: extraqct assetUrl for readability

--- src/components/BorderDecoration.tsx
+++ src/components/BorderDecoration.tsx
@@ -22,40 +22,41 @@ export const BorderDecoration = ({
     rightEdge,
   } = borderStyles;
 
+  const assetUrl = `${import.meta.env.BASE_URL}png`;
+
   return (
     <div className="container">
       <div
         className="upper left corner"
-        // style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${upperLeftCorner}.png)` }}
-        style={{ backgroundImage: `url(../png/${upperLeftCorner}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${upperLeftCorner}.png)` }}
       ></div>
       <div
         className="upper right corner"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${upperRightCorner}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${upperRightCorner}.png)` }}
       ></div>
       <div
         className="lower left corner"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${lowerLeftCorner}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${lowerLeftCorner}.png)` }}
       ></div>
       <div
         className="lower right corner"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${lowerRightCorner}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${lowerRightCorner}.png)` }}
       ></div>
       <div
         className="upper edge"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${upperEdge}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${upperEdge}.png)` }}
       ></div>
       <div
         className="lower edge"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${lowerEdge}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${lowerEdge}.png)` }}
       ></div>
       <div
         className="left edge"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${leftEdge}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${leftEdge}.png)` }}
       ></div>
       <div
         className="right edge"
-        style={{ backgroundImage: `url(${import.meta.env.BASE_URL}png/${rightEdge}.png)` }}
+        style={{ backgroundImage: `url(${assetUrl}/${rightEdge}.png)` }}
       ></div>
       {children}
     </div>
